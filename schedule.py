6eea1bd7b6208d3dc765f0b01c120f5515d0ce2c
Greg Swift <gregswift@gmail.com>
2021-03-15 08:20:55 -0500

    feat: support only enabling certain saturdays

--- schedule.py
+++ schedule.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import datetime
 import os
 import pprint
 import sys
@@ -21,6 +22,42 @@ SATURDAY_SCHEDULE={'schedule': {'weeklyRanges': {
     'friday':    [{'from': '08:30', 'to': '17:30'}],
     'saturday':  [{'from': '08:30', 'to': '17:30'}]
 }}}
+
+# List of which saturday of the month to enable
+WHICH_SATURDAYS=[3]
+
+SATURDAY=5 # datetime.date().weekday for Saturday
+
+def this_saturday(now=None):
+    """ Return integer representing which saturday of the month
+        the upcoming saturday is
+    """
+    if now is None:
+        now = datetime.datetime.now()
+    day = datetime.date(now.year, now.month, now.day)
+    weekday = day.weekday()
+    if weekday > SATURDAY:
+        # Sunday is the only day larger than Saturday from datetime.date().weekday
+        # and its also the number to add to timeshift us to the next
+        # Saturday
+        timeshift = weekday
+        #print(f'Days till next Saturday: {timeshift}')
+    else:
+        # The difference is how many days till saturday
+        timeshift = SATURDAY - weekday
+        #print(f'Days till next Saturday ({SATURDAY} - {weekday}): {timeshift}')
+    saturday = day + datetime.timedelta(days=timeshift)
+    #print(saturday)
+    if saturday.day >= 1 and saturday.day <= 7:
+        return 1
+    if saturday.day >= 8 and saturday.day <= 14:
+        return 2
+    if saturday.day >= 15 and saturday.day <= 21:
+        return 3
+    if saturday.day >= 22 and saturday.day <= 28:
+        return 4
+    if saturday.day >= 29 and saturday.day <= 31:
+        return 5
 
 def get_config():
     return {
@@ -65,8 +102,10 @@ if __name__ == '__main__':
             if sys.argv[1] == 'get':
                 pprint.pprint(get_schedule(api,config))
             if sys.argv[1] == 'enable-saturdays':
+                if this_saturday() not in WHICH_SATURDAYS:
+                    break
                 pprint.pprint(set_schedule(api,config,SATURDAY_SCHEDULE))
-            if sys.argv[1] in ['standard','disable-saturdays']:
+            if sys.argv[1] in ['standard', 'disable-saturdays']:
                 pprint.pprint(set_schedule(api,config,STANDARD_SCHEDULE))
             x += 1
         if 'help' in sys.argv[1]:
